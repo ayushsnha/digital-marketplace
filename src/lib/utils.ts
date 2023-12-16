5f0b91453eca566a166cab74791efb97fdd4df78
ayushsnha <ayush_snha@rediffmail.com>
2023-12-16 21:01:51 +0530

    cart init

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,21 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs))
 }
+
+export function formatPrice(
+    price: number | string,
+    options:{
+        currency?: 'USD' | 'EUR' | 'GBP' | 'INR',
+        notation?: Intl.NumberFormatOptions['notation']
+    }={}
+) {
+    const { currency= 'INR', notation='standard' } = options
+    const numericPrice = typeof price === 'string' ? parseFloat(price) : price;
+
+    return new Intl.NumberFormat('en-IN', {
+        currency,
+        style: 'currency',
+        notation,
+        maximumFractionDigits: 2
+    }).format(numericPrice)
+}
